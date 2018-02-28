4f2d8c5fc9b55a963ec0f9529b825df75ff3b965
Darko Sos <darko.sos@fincore.com>
2018-02-28 09:29:55 +0100

    new files 28/02/2018

---
+++ parallelSort/first_test.go
@@ -0,0 +1,7 @@
+package main
+
+import "testing"
+
+func TestTimeConsuming(t *testing.T) {
+	DoIt(15000000, 15000)
+}
--- parallelSort/parallel-sort.go
+++ parallelSort/parallel-sort.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"sort"
+	"time"
 )
 
+/*
+func inPlaceMerge(mergeTo []int, mergeFrom []int) {
+	i := 0
+	j := 0
+}
+*/
 // merges two sorted sequences into new sorted one
 func merge(a []int, b []int) (c []int) {
 	i := 0
@@ -12,14 +21,17 @@ func merge(a []int, b []int) (c []int) {
 	c = make([]int, len(a)+len(b))
 	// run until both sequences are read
 	for i < len(a) || j < len(b) {
+		// if a is read, fill rest with b
 		if i >= len(a) {
 			c[i+j] = b[j]
 			j++
 		} else {
+			// if b is read, fill rest with a
 			if j >= len(b) {
 				c[i+j] = a[i]
 				i++
 			} else {
+				// if both are not read, compare and move one that has smaller value
 				if a[i] < b[j] {
 					c[i+j] = a[i]
 					i++
@@ -29,15 +41,55 @@ func merge(a []int, b []int) (c []int) {
 				}
 			}
 		}
-
 	}
-	fmt.Printf("i=%d, j=%d", i, j)
 	return
 }
 
+func smallSort(a []int, ss chan []int) {
+	// here we use go's sort package
+	sort.Ints(a)
+	ss <- a
+}
+
+func createRandomArray(nSize int) (b []int) {
+	b = make([]int, nSize)
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < nSize; i++ {
+		b[i] = rand.Intn(10000) - 500
+	}
+	// returns named return parameter b
+	return
+}
+
+// DoIt will do it
+func DoIt(nSize, nChunk int) {
+	ar := createRandomArray(nSize)
+	arrayChannel := make(chan []int)
+	//fmt.Println("Original array:")
+	// fmt.Println(ar)
+	var sortedArray []int
+	startTime := time.Now()
+	fmt.Printf("starting go routines at %v\n", startTime)
+	for i := 0; i < nSize; i += nChunk {
+		// in place sort of subarray
+		go smallSort(ar[i:i+nChunk], arrayChannel)
+	}
+	fmt.Printf("receiving go routines at %v\n", time.Now())
+	for i := 0; i < nSize; i += nChunk {
+		sortedSubarray := <-arrayChannel
+		fmt.Printf("chunk received at %v\n", time.Now())
+		if i == 1 {
+			sortedArray = sortedSubarray
+		} else {
+			// merge so far sorted array with new chunk
+			sortedArray = merge(sortedArray, sortedSubarray)
+		}
+	}
+	endTime := time.Now()
+	fmt.Printf("all done at %v\n", endTime)
+	fmt.Printf("execution time = %v\n", time.Duration(endTime.UnixNano()-startTime.UnixNano()))
+}
+
 func main() {
-	a := []int{1, 2, 4, 10}
-	b := []int{3, 5, 7}
-	c := merge(a, b)
-	fmt.Println(c)
+	DoIt(15000000, 15000)
 }
